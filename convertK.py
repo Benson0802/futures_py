349c0f63c80183ed4ba11131ede09ef52d4ce8b7
HuiMing <ice00083@gmail.com>
2023-02-25 22:33:32 +0800

    1.原先的方式先註解掉(tick轉為分k會有誤) 2.增加history的寫入及轉分k(先註解有需要再打開)

--- convertK.py
+++ convertK.py
@@ -1,49 +1,84 @@
 import pandas as pd
 import os.path
+import csv
 
 class convertK():
     '''
     tick轉換為k棒
     '''
     def __init__(self, tick):
-        self.datetime = tick['datetime']
-        self.open = float(tick['open'])
-        self.high = float(tick['high'])
-        self.low = float(tick['low'])
-        self.volume = int(tick['volume'])
-        self.close = float(tick['close'])
-        # 將tick轉換為DataFrame
-        self.df = pd.DataFrame({'datetime': [self.datetime], 'open': [self.open], 'high': [self.high], 'low': [self.low], 'volume': [self.volume], 'close':[self.close]})
-        # 轉換時間戳
-        self.df['datetime'] = pd.to_datetime(self.df['datetime'], format='%Y-%m-%d %H:%M:%S.%f')
-        # 設置時間戳為索引
-        self.df.set_index('datetime', inplace=True)
+        # self.datetime = tick['datetime']
+        # self.open = float(tick['open'])
+        # self.high = float(tick['high'])
+        # self.low = float(tick['low'])
+        # self.volume = int(tick['volume'])
+        # self.close = float(tick['close'])
+        self.tick_path = 'data/tick.csv'
+        self.min_path = 'data/1Min.csv'
+        #self.tick = tick
+        #self.write_tick()
     
+    def write_tick(self):
+        '''
+        將k棒寫入csv
+        '''
+        
+    def write_history(self):
+        df = pd.DataFrame({**self.tick})
+        df.ts = pd.to_datetime(df.ts)
+        df.to_csv(self.tick_path, mode='a', index=False, header=not os.path.exists(self.tick_path))
+        
     def convert_k_bar(self,time_unit):
         '''
-        tick轉為k棒
+        將歷史分k轉為n分
         '''
         file_path = os.path.join('data', time_unit + '.csv')
+        df = pd.read_csv(self.min_path)
+        df['ts'] = pd.to_datetime(df['ts'])
+        df = df.set_index('ts')
+        ohlc_dict = {
+            'Open': 'first',
+            'High': 'max',
+            'Low': 'min',
+            'Close': 'last',
+            'Volume': 'sum'
+        }
+        df_data = df.resample(time_unit).apply(ohlc_dict)
+        df_data = df_data.dropna()
+        df_data.to_csv(file_path)
+        print(df_data)
+        
+    # def convert_k_bar(self,time_unit):
+    #     '''
+    #     tick轉為k棒
+    #     '''
+    #     # 將tick轉換為DataFrame
+    #     self.df = pd.DataFrame({'Open': [self.open], 'ts': [self.datetime], 'High': [self.high] , 'Volume': [self.volume], 'Low': [self.low], 'Amount': [self.low], 'Close':[self.close]})
+    #     # 轉換時間戳
+    #     self.df['datetime'] = pd.to_datetime(self.df['ts'], format='%Y-%m-%d %H:%M:%S.%f')
+    #     # 設置時間戳為索引
+    #     self.df.set_index('datetime', inplace=True)
+    #     file_path = os.path.join('data', time_unit + '.csv')
+        
+    #     # 判斷文件是否為空或是沒文件則先創造DataFrame
+    #     if not os.path.isfile(file_path) or os.stat(file_path).st_size == 0:
+    #         empty_df = pd.DataFrame(columns=['Open', 'ts', 'High', 'Volume', 'Low', 'Amount','Close'])
+    #         empty_df.to_csv(file_path)
 
-        # 判斷文件是否為空或是沒文件則先創造DataFrame
-        if not os.path.isfile(file_path) or os.stat(file_path).st_size == 0:
-            empty_df = pd.DataFrame(columns=['open', 'high', 'low', 'close'])
-            empty_df.to_csv(file_path)
+    #     # 讀取csv
+    #     existing_df = pd.read_csv(file_path, index_col=0, parse_dates=True)
 
-        # 讀取csv
-        existing_df = pd.read_csv(file_path, index_col=0, parse_dates=True)
+    #     # 排除重覆資料
+    #     if len(existing_df) > 0 and existing_df.index[-1] == self.df.resample(time_unit).apply({'Open': 'first', 'High': 'max', 'Low': 'min', 'Close': 'last'}).dropna().index[0]:
+    #         print('Duplicate data. Skipping...')
+    #         return False
 
-        # 排除重覆資料
-        if len(existing_df) > 0 and existing_df.index[-1] == self.df.resample(time_unit).apply({'open': 'first', 'high': 'max', 'low': 'min', 'close': 'last'}).dropna().index[0]:
-            print('Duplicate data. Skipping...')
-            return False
+    #     # 讀取新資料
+    #     kbar = self.df.resample(time_unit).apply({'Open': 'first', 'High': 'max', 'Low': 'min', 'Close': 'last'}).dropna()
 
-        # 讀取新資料
-        kbar = self.df.resample(time_unit).apply({'open': 'first', 'high': 'max', 'low': 'min', 'close': 'last'}).dropna()
+    #     # 合併現有資料並寫入csv
+    #     new_df = pd.concat([existing_df, kbar])
+    #     new_df.to_csv(file_path)
 
-        # 合併現有資料並寫入csv
-        new_df = pd.concat([existing_df, kbar])
-        new_df.to_csv(file_path)
-
-        print(kbar)
-        return True
+    #     print(kbar)
+    #     return True
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import shioaji as sj
 from shioaji import TickFOPv1, Exchange
 import threading
 from check_opening import check_opening
-import os.path
 import json
 from convertK import convertK
 
@@ -12,34 +11,40 @@ flag = obj.check_date()
 year_mon = obj.get_year_mon()
 code = 'MXF' + str(year_mon['year']) + ('0' if len(str(year_mon['mon'])) == 1 else '') + str(year_mon['mon'])
 
-if flag is True:
-    with open('API_KEY.json', 'r') as f:
-        json_data = json.load(f)
+# 登入
+with open('API_KEY.json', 'r') as f:
+    json_data = json.load(f)
     api = sj.Shioaji()
     api.login(
         api_key=json_data['api_key'],
         secret_key=json_data['secret_key'],
         contracts_cb=lambda security_type: print(f"{repr(security_type)} fetch done.")
     )
+
+#開盤時間抓tick
+if flag is True:
     api.quote.subscribe(
         api.Contracts.Futures.MXF[code],
         quote_type = sj.constant.QuoteType.Tick,
         version = sj.constant.QuoteVersion.v1,
     )
-
     @api.on_tick_fop_v1()
     def quote_callback(exchange:Exchange, tick:TickFOPv1):
         ck = convertK(tick)
-        ck.convert_k_bar("1Min")
-        ck.convert_k_bar("5Min")
-        ck.convert_k_bar("15Min")
-        ck.convert_k_bar("30Min")
-        ck.convert_k_bar("60Min")
-        ck.convert_k_bar("D")
-        ck.convert_k_bar("W")
-        ck.convert_k_bar("M")
-        
-        
-threading.Event().wait()
+        # ck.convert_k_bar("1Min")
+        # ck.convert_k_bar("5Min")
+        # ck.convert_k_bar("60Min")
+#非開盤時間抓歷史資料
+else:
+    # kbars = api.kbars(
+    #     contract=api.Contracts.Futures.MXF[code], 
+    #     start='2023-01-02',
+    #     end='2023-02-25',
+    # )
+    # ck = convertK(kbars)
+    # ck.convert_k_bar('5Min')
+    # ck.convert_k_bar('60Min')
+    
+#threading.Event().wait()
 
-api.logout()
+#api.logout()
