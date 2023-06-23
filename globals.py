c4a8eb6bd2eead71c4062cca146e9071719cf6ca
HuiMing <ice00083@gmail.com>
2023-06-23 11:44:40 +0800

    tick改為讀取kbar

--- globals.py
+++ globals.py
@@ -11,6 +11,8 @@ def initialize():
     global how #取幾根k棒
     global levels #支撐壓力
     global aisle_type #判斷類型 1上通道 2下通道 3 最高壓力  4最低支撐  5次高壓力 6次低支撐
+    global pattern
+    global today
     has_thread = False
     now_min = None
     tick_min = None
@@ -23,3 +25,5 @@ def initialize():
     levels = None
     direction = 0
     aisle_type = 0
+    pattern = None
+    today = None
