bd18771211e14882854d0d2be4e1635a5e31a282
HuiMing <ice00083@gmail.com>
2023-07-16 12:29:14 +0800

    1.改回tick 2.更新k棒資料

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import globals
 import time
 import pandas as pd
 from cls.tactics.indicator import indicator
+import logging
 
 globals.initialize()
 obj = check_opening()
@@ -26,95 +27,98 @@ with open('API_KEY.json', 'r') as f:
         contracts_cb=lambda security_type: print(f"{repr(security_type)} fetch done.")
     )
     
-    while (True):
-        #改抓kbar
-        current_time = datetime.datetime.now()
-        start_time = current_time.replace(hour=8, minute=45)
-        end_time = current_time.replace(hour=13, minute=45)
-        next_day_time = current_time.replace(hour=5, minute=0) + datetime.timedelta(days=1)
-        if start_time <= current_time <= end_time or current_time >= current_time.replace(hour=15, minute=0) or current_time <= next_day_time:
-            globals.today = datetime.datetime.now().date().strftime('%Y-%m-%d')
-            kbars = api.kbars(
-                contract=api.Contracts.Futures.TXF.TXFR1,
-                start=globals.today,
-                end=globals.today,
-            )
+    # while (True):
+    #     #改抓kbar
+    #     current_time = datetime.datetime.now()
+    #     start_time = current_time.replace(hour=8, minute=45)
+    #     end_time = current_time.replace(hour=13, minute=45)
+    #     next_day_time = current_time.replace(hour=5, minute=0) + datetime.timedelta(days=1)
+    #     if start_time <= current_time <= end_time or current_time >= current_time.replace(hour=15, minute=0) or current_time <= next_day_time:
+    #         globals.today = datetime.datetime.now().date().strftime('%Y-%m-%d')
+    #         logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO)
+    #         logging.info(current_time)
+    #         kbars = api.kbars(
+    #             contract=api.Contracts.Futures.TXF.TXFR1,
+    #             start=globals.today,
+    #             end=globals.today,
+    #             timeout = 30000
+    #         )
             
-            ck = convertK(kbars)
-            ck.write_history_1k_bar()
+    #         ck = convertK(kbars)
+    #         ck.write_history_1k_bar()
+    #         ck.convert_k_bar('5Min')
+    #         ck.convert_k_bar('15Min')
+    #         ck.convert_k_bar('30Min')
+    #         ck.convert_k_bar('60Min')
+    #         ck.convert_day_k_bar()
+    #         last_close = ck.get_last_close()
+    #         if last_close is not None:
+    #             ord = indicator(last_close)
+    #             ord.run(5)
+    #             # tactics = aisle(last_close)
+    #             # tactics.run(5)
+    #             # ord = order(last_close)
+    #             # ord.strategy2(60)
+    #         time.sleep(10)
+
+    #開盤時間抓tick
+    api.quote.subscribe(
+        api.Contracts.Futures.TXF[globals.code],
+        quote_type = sj.constant.QuoteType.Tick,
+        version = sj.constant.QuoteVersion.v1,
+    )
+            
+    @api.on_tick_fop_v1()
+
+    def quote_callback(exchange:Exchange, tick:TickFOPv1):
+        if tick.simtrade == True: return #避開試搓時間
+        ck = convertK(tick,True)
+        
+        #最後一盤資料寫入csv
+        current_time = datetime.datetime.now().time().replace(second=0, microsecond=0)
+        if current_time == datetime.time(hour=4, minute=59) or current_time == datetime.time(hour=13, minute=44):
+            ck.write_tick("tick")
+            #最後一盤資料寫入各分k
+            if current_time == datetime.time(hour=5, minute=0) or current_time == datetime.time(hour=13, minute=45):
+                df_tick = pd.read_csv('data/tick.csv', index_col='datetime')
+                for index, row in df_tick.iterrows():
+                    globals.amount.append(row['close'])
+                    globals.volume += row['volume']
+                            
+                now = datetime.datetime.now()
+                last_min  = now.strftime('%Y/%m/%d %H:%M:S')
+                ck = convertK(tick,True)
+                ck.write_1k_bar(last_min,globals.volume,globals.amount)
+                ck.convert_k_bar('5Min')
+                ck.convert_k_bar('15Min')
+                ck.convert_k_bar('30Min')
+                ck.convert_k_bar('60Min')
+                ck.convert_day_k_bar()
+        
+        globals.now_min = ck.get_now_min()
+        globals.tick_min = ck.get_tick_min()
+        
+        if globals.now_min == globals.tick_min: #現在的分鐘數與tick分鐘相符合就收集資料
+            globals.volume += tick.volume
+            if tick.close not in globals.amount: #排除重覆資料
+                globals.amount.append(tick.close)
+        else:
+            ck.write_1k_bar(globals.tick_min,globals.volume,globals.amount)
+            #策略判斷
+            # tactics = aisle(tick.close)
+            # tactics.run(5)
+            # ord = order(tick.close)
+            # ord.strategy2(60)
+            ord = indicator(tick.close)
+            ord.run(5)
+            globals.now_min = None
+            globals.amount.clear()
+            globals.volume = tick.volume
             ck.convert_k_bar('5Min')
             ck.convert_k_bar('15Min')
             ck.convert_k_bar('30Min')
             ck.convert_k_bar('60Min')
             ck.convert_day_k_bar()
-            last_close = ck.get_last_close()
-            if last_close is not None:
-                ord = indicator(last_close)
-                ord.run(5)
-                # tactics = aisle(last_close)
-                # tactics.run(5)
-                # ord = order(last_close)
-                # ord.strategy2(60)
-            time.sleep(10)
 
-# #開盤時間抓tick
-# api.quote.subscribe(
-#     api.Contracts.Futures.TXF[globals.code],
-#     quote_type = sj.constant.QuoteType.Tick,
-#     version = sj.constant.QuoteVersion.v1,
-# )
-        
-# @api.on_tick_fop_v1()
-
-# def quote_callback(exchange:Exchange, tick:TickFOPv1):
-#     if tick.simtrade == True: return #避開試搓時間
-#     ck = convertK(tick,True)
-    
-#     #最後一盤資料寫入csv
-#     current_time = datetime.datetime.now().time().replace(second=0, microsecond=0)
-#     if current_time == datetime.time(hour=4, minute=59) or current_time == datetime.time(hour=13, minute=44):
-#         ck.write_tick("tick")
-#         #最後一盤資料寫入各分k
-#         if current_time == datetime.time(hour=5, minute=0) or current_time == datetime.time(hour=13, minute=45):
-#             df_tick = pd.read_csv('data/tick.csv', index_col='datetime')
-#             for index, row in df_tick.iterrows():
-#                 globals.amount.append(row['close'])
-#                 globals.volume += row['volume']
-                        
-#             now = datetime.datetime.now()
-#             last_min  = now.strftime('%Y/%m/%d %H:%M:S')
-#             ck = convertK(tick,True)
-#             ck.write_1k_bar(last_min,globals.volume,globals.amount)
-#             ck.convert_k_bar('5Min')
-#             ck.convert_k_bar('15Min')
-#             ck.convert_k_bar('30Min')
-#             ck.convert_k_bar('60Min')
-#             ck.convert_day_k_bar()
-    
-#     globals.now_min = ck.get_now_min()
-#     globals.tick_min = ck.get_tick_min()
-    
-#     if globals.now_min == globals.tick_min: #現在的分鐘數與tick分鐘相符合就收集資料
-#         globals.volume += tick.volume
-#         if tick.close not in globals.amount: #排除重覆資料
-#             globals.amount.append(tick.close)
-#     else:
-#         ck.write_1k_bar(globals.tick_min,globals.volume,globals.amount)
-#         #策略判斷
-#         # tactics = aisle(tick.close)
-#         # tactics.run(5)
-#         # ord = order(tick.close)
-#         # ord.strategy2(60)
-#         ord = indicator(tick.close)
-#         ord.run(5)
-#         globals.now_min = None
-#         globals.amount.clear()
-#         globals.volume = tick.volume
-#         ck.convert_k_bar('5Min')
-#         ck.convert_k_bar('15Min')
-#         ck.convert_k_bar('30Min')
-#         ck.convert_k_bar('60Min')
-#         ck.convert_day_k_bar()
-
-# threading.Event().wait()
-# api.logout()
+    threading.Event().wait()
+    api.logout()
