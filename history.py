399f70414fba5935e612fb88a789a2ab14435fde
HuiMing <ice00083@gmail.com>
2024-07-24 09:20:57 +0800

    更新k棒

--- history.py
+++ history.py
@@ -20,8 +20,8 @@ with open('API_KEY.json', 'r') as f:
 
     kbars = api.kbars(
         contract=api.Contracts.Futures.TXF.TXFR1,
-        start='2024-06-24',
-        end='2024-07-06',
+        start='2024-07-08',
+        end='2024-07-24',
     ) 
     ck = convertK(kbars)
     ck.write_history_1k_bar()
